7f5a44db87a0ae7f493b819501d6c8adc7d205bf
john <john@chinawebeye.com>
2015-12-26 20:51:03 +0800

    study 1

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -7,5 +7,24 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	greeting := "hello john!!!"
+	return c.Render(greeting)
+}
+
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("Your name is required!")
+	c.Validation.MinSize(myName, 3).Message("Your name is not long enough!")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
+	return c.Render(myName)
+}
+
+func (c App) Register() revel.Result {
+	title := "register"
+	return c.Render(title)
 }
