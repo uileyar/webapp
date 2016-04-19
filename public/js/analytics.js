d7969762e7c3013baab1d30de9a5109695752795
uileyar <85773031@qq.com>
2016-04-20 00:03:31 +0800

    add analytics 1

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,83 +1,80 @@
-$(document).ready(function () {
-    Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function (color) {
-        return {
-            radialGradient: {cx: 0.5, cy: 0.3, r: 0.7},
-            stops: [
-                [0, color],
-                [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
-            ]
-        };
-    });
-
-    $.get("/Accounts/CsvData?t=expense", function (csv) {
-        $('#container-ae').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: '账户支出汇总'
-            },
-            credits: {
-                enabled: false
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>￥{point.y};{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b>:{point.percentage:.1f}%<br/>￥{point.y}',
-                        style: {
-                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+var get_highcharts = function (tab_id) {
+    if (tab_id == "bills-tab") {
+        $.get("/Accounts/CsvData?t=expense", function (csv) {
+            $('#container-bm').highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: '账户支出汇总'
+                },
+                credits: {
+                    enabled: false
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>￥{point.y};{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>:{point.percentage:.1f}%<br/>￥{point.y}',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            }
                         }
                     }
+                },
+                data: {
+                    csv: csv
                 }
-            },
-            data: {
-                csv: csv
-            }
+            });
         });
-    });
 
-    $.get("/Accounts/CsvData?t=income", function (csv) {
-        $('#container-ai').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: '账户收入汇总'
-            },
-            credits: {
-                enabled: false
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>￥{point.y};{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b>:{point.percentage:.1f}%<br/>￥{point.y}',
-                        style: {
-                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+        $.get("/Accounts/CsvData?t=income", function (csv) {
+            $('#container-by').highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: '账户收入汇总'
+                },
+                credits: {
+                    enabled: false
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>￥{point.y};{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>:{point.percentage:.1f}%<br/>￥{point.y}',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            }
                         }
                     }
+                },
+                data: {
+                    csv: csv
                 }
-            },
-            data: {
-                csv: csv
-            }
+            });
         });
-    });
-});
+    } else if (tab_id == "account-tab") {
+        alert(tab_id)
+    } else if (tab_id == "catelog-tab") {
+        alert(tab_id)
+    }
+}
+
